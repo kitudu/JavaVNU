fd06a3f2094914629a5b2bd82e5b0454430407a7
kieutuandung-aptech-vn <fc.ktdung@aptech.vn>
2023-03-16 15:49:55 +0700

    Prac04 Less01 solution

---
+++ Prac04Less01/src/main/java/vn/edu/vnu/java/prac04less01/Prac04Less01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vn.edu.vnu.java.prac04less01;
+
+/**
+ *
+ * @author kieut
+ */
+public class Prac04Less01 {
+
+    public static void main(String[] args) {
+        System.out.println("Area = ");
+        System.out.println(4.5 * 7.9);
+        System.out.println("Perimeter = ");
+        System.out.println((4.5 + 7.9) * 2);
+    }
+}
