cd44fb0f7bb3e498b8ac488139d8ac689fd5a8f0
kieutuandung-aptech-vn <fc.ktdung@aptech.vn>
2023-03-17 05:24:36 +0700

    Prac01 Less05 solution

---
+++ Prac01Less05/src/prac01less05/Prac01Less05.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prac01less05;
+
+/**
+ *
+ * @author kieut
+ */
+public class Prac01Less05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sum = 0;
+        //Vòng lặp for bắt đầu và chạy cho tới x=20
+        for (int x = 1; x <= 20; x++) {
+            sum = sum + x;
+        }
+        System.out.println("Sum: " + sum);
+    }
+    
+}
