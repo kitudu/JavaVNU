ece4a7a29e762b2d2373a4571a3dea44dbd538b4
kieutuandung-aptech-vn <fc.ktdung@aptech.vn>
2023-03-16 11:34:53 +0700

    Prac02 Less01 solution

---
+++ Prac02Less01/src/main/java/vn/edu/vnu/java/prac02less01/Prac02Less01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vn.edu.vnu.java.prac02less01;
+
+/**
+ *
+ * @author kieut
+ */
+public class Prac02Less01 {
+
+    public static void main(String[] args) {
+        System.out.println("    J     A    V     V    A");
+        System.out.println("    J    A A    V   V    A A");
+        System.out.println("J   J   AAAAA    V V    AAAAA");
+        System.out.println(" J J   A     A    V    A     A");
+    }
+}
