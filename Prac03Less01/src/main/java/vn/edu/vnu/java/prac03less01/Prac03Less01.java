e76c500117256651bb22ca464a94ac405c8225ae
kieutuandung-aptech-vn <fc.ktdung@aptech.vn>
2023-03-16 15:42:28 +0700

    Prac03 Less01 Solution

---
+++ Prac03Less01/src/main/java/vn/edu/vnu/java/prac03less01/Prac03Less01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vn.edu.vnu.java.prac03less01;
+
+/**
+ *
+ * @author kieut
+ */
+public class Prac03Less01 {
+
+    public static void main(String[] args) {
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    }
+}
